3701f35bb0300bb9c02b9564afdf31ee2696f7e4
Atif Hameed <atifhameed11312@gmail.com>
2025-06-25 13:36:51 +0500

    fix deploy

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,3 @@
 // export const backend_url = 'http://localhost:5400';
-export const backend_url = 'https://social-media-backend-zysoftec.vercel.app';
+// export const backend_url = 'https://social-media-backend-zysoftec.vercel.app';
+export const backend_url = 'https://social-server-b1l6.vercel.app'
